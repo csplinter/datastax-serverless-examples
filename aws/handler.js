76c6d5059fd1360d3d91cf9f40003040a74c1b2a
csplinter <chris.splinter@datastax.com>
2019-09-18 11:31:57 -0500

    disable heartbeat, remove metadata refresh, set idempotence for writes, add wait for event loop, add logging

--- aws/handler.js
+++ aws/handler.js
@@ -11,12 +11,24 @@ if (!localDataCenter) throw new Error('Environment variable LOCAL_DC not set');
 
 const client = new cassandra.Client({
   contactPoints: contactPoints.split(','),
-  localDataCenter
-  // If trying to reduce Cold Start time, 
-  // the driver's automatic metadata synchronization and pool warmup can be disabled 
-  //
+  localDataCenter,
+
+  // AWS Lambda freezes the function execution context after the callback has been invoked. 
+  // This means that no background activity can occur between lambda invocations, 
+  // including the heartbeat that the driver uses to prevent idle disconnects in some environments.
+  // For this reason, we disable it below.
+  pooling: { heartBeatInterval: 0 }
+
+  // If trying to reduce Cold Start time, the driver's automatic metadata synchronization and pool warmup can be disabled 
   // isMetadataSyncEnabled: false,
   // pooling: { warmup: false }
+});
+
+// Enable logging for the purpose of example
+client.on('log', (level, className, message) => {
+  if (level !== 'verbose') {
+    console.log('Driver log event', level, className, message);
+  }
 });
 
 const createKeyspace = `CREATE KEYSPACE IF NOT EXISTS ${keyspace} ` +
@@ -30,7 +42,6 @@ const writeQuery = `INSERT INTO ${keyspace}.${table} (item_id, name, description
 const readQuery = `SELECT name, description, price FROM ${keyspace}.${table} WHERE item_id = ?`;
 
 client.connect()
-  .then(() => client.metadata.refreshKeyspace(keyspace))
   .then(() => console.log('Connected to the DSE cluster, discovered %d nodes', client.hosts.length))
   .catch(err => console.error('There was an error trying to connect', err));
 
@@ -42,7 +53,7 @@ async function createSchema() {
 
 async function addItem(item_id, name, description, price) {
   const params = [ item_id, name, description, price ];
-  await client.execute(writeQuery, params, { prepare : true });
+  await client.execute(writeQuery, params, { prepare: true, isIdempotent: true });
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -71,15 +82,18 @@ async function getItem(item_id) {
   };
 }
 
-module.exports.createCatalog = async event => {
+module.exports.createCatalog = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   return createSchema();
 };
 
-module.exports.addItem = async (event) => {
+module.exports.addItem = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   const data = JSON.parse(event.body);
   return addItem(data.item_id, data.name, data.description, data.price);
 };
 
-module.exports.getItem = async (event) => {
+module.exports.getItem = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   return getItem(event.pathParameters.id);
 };
